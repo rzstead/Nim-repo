8976c741330bb0023be56b97553182cc6f671687
rzstead <rzstea@gmail.com>
2016-10-13 08:46:58 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master' into SteadBranch
    # Conflicts:
    #	Nim/Nim/CpuVsCpu/Game.cs

--- Nim/NimTester/NimTests.cs
+++ Nim/NimTester/NimTests.cs
@@ -4,12 +4,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace NimTester
 {
     using Nim;
+    using Nim.CpuVsCpu;
+    using Nim.Players;
+    using System.Collections.Generic;
     [TestClass]
     public class NimTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckProperAISelection()
         {
+            char[][] visualEdit = new char[3][]
+        {
+            new char[] {'o', 'o', 'o'},
+            new char[] {'x', 'x', 'x', 'o', 'o'},
+            new char[] {'o', 'o', 'o', 'o', 'o', 'o', 'o'}
+        };
+            char[][] visual = new char[3][]
+{
+            new char[] {'o', 'o', 'o'},
+            new char[] {'o', 'o', 'o', 'o', 'o'},
+            new char[] {'o', 'o', 'o', 'o', 'o', 'o', 'o'}
+};
+            int[] move = { 2, 3 };
+            State state = new State(move, .5, visualEdit);
+            List<State> prevStates = new List<State>();
+
+            Player p = new LearnCPU(visual);
+            ((LearnCPU)p).AddMove(state);
+            p.ChooseMove();
+
+
         }
     }
 }
